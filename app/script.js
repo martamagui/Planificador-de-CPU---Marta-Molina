eb0fd21c4c6dadc9c7456220f177d3559a72e551
Marta Molina <marta.molina@live.u-tad.com>
2021-05-26 18:56:26 +0200

    Comienzo de la segunda pantalla

--- app/script.js
+++ app/script.js
@@ -1,32 +1,34 @@
-/*Autor= Marta Molina*/
+/** 
+ * @Autor= Marta Molina Aguilera
+ * */
 let tarjeta = document.getElementById("tarjeta");
-
-
-//CARGA INICIAL
-function cargarIncial() {
-  let explicacion = document.createElement("h3");
-  explicacion.innerHTML =
-    "Lo primero que deberás hacer es seleccionar qué algoritmo te gustaría que organice la ejeccución de tus procesos:";
-  let botones = document.createElement("div");
-  botones.setAttribute("id", "sub_tarjeta");
-  tarjeta.appendChild(explicacion);
-  botones.innerHTML =
-    '<div id="packFifo" class="pack"><button onclick="vaciarTarjeta("FIFO")">FIFO</button><span>El primer proceso en llegar, será el priemro en ser atendido</span></div>'+
-    '<div id="packSJF" class="pack"><button onclick="vaciarTarjeta("SJF")">SJF</button><span>El más corto será el primero en ser atendido.</span></div>'+
-    '<div id="packRRobin" class="pack"><button onclick="vaciarTarjeta("Round Robin")">Round Robin</button><span>Se establece un intervalo de tiempo a cada proceso. Una vez este es agotado, pasa al siguiente.</span></div>';
-tarjeta.appendChild(explicacion);
-tarjeta.appendChild(botones);
-}
+let subTarjeta = document.createElement("div");
+subTarjeta.setAttribute("class", "sub_tarjeta en_columns");
+ 
 //VACIAR Y PASAR A LOS CAMPOS DE TEXTO
-function vaciarTarjeta(param) {
-    if(param=="FIFO"){
+function mostrarForm(param) {
+  tarjeta.innerHTML="";
+  tarjeta.appendChild(subTarjeta);
+  let titulo= document.createElement("h1");
+  titulo.setAttribute("class", "st_titulo");
 
+  let explicacion=document.createElement("span");
+  explicacion.setAttribute("class","st_texto");
+
+    if(param=="FIFO"){
+      titulo.innerHTML="FIFO";
+      explicacion.innerHTML="El primer proceso en llegar, será el primero en ser atendido.";
+      
     }else if(param=="SJF"){
+      titulo.innerHTML="SJF (Shortest Job First)";
+      explicacion.innerHTML="El más corto será el primero en ser atendido.";
 
     }else if(param=="Round Robin"){
-
+      titulo.innerHTML="Round Robin";
+      explicacion.innerHTML="Se establece un intervalo de tiempo a cada proceso. Una vez este es agotado, pasa al siguiente.";
     }else{}
-    tarjeta.innerHTML="";
+    subTarjeta.appendChild(titulo);
+    subTarjeta.appendChild(explicacion);
 }
 
 
