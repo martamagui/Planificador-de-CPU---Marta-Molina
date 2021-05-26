e30adc8d888b0a4188fb918d9c3a70288eb9efb5
Marta Molina <marta.molina@live.u-tad.com>
2021-05-26 20:53:19 +0200

    Ya carga la 2ªpantalla según el algoritmo elegido

--- app/script.js
+++ app/script.js
@@ -1,53 +1,84 @@
-/** 
+/**
  * @Autor= Marta Molina Aguilera
  * */
 let tarjeta = document.getElementById("tarjeta");
 let subTarjeta = document.createElement("div");
 subTarjeta.setAttribute("class", "sub_tarjeta en_columns");
- 
+
 //VACIAR Y PASAR A LOS CAMPOS DE TEXTO
 function mostrarForm(param) {
-  tarjeta.innerHTML="";
+  console.log(param);
+  tarjeta.innerHTML = "";
+  let num;
+  let campos = ["Llegada", "Duracion", "Qtum"];
   tarjeta.appendChild(subTarjeta);
-  let titulo= document.createElement("h1");
+  let titulo = document.createElement("h1");
   titulo.setAttribute("class", "st_titulo");
 
-  let explicacion=document.createElement("span");
-  explicacion.setAttribute("class","st_texto");
+  let explicacion = document.createElement("span");
+  explicacion.setAttribute("class", "st_texto");
+  let formulario = document.createElement("form");
 
-    if(param=="FIFO"){
-      titulo.innerHTML="FIFO";
-      explicacion.innerHTML="El primer proceso en llegar, será el primero en ser atendido.";
-      
-    }else if(param=="SJF"){
-      titulo.innerHTML="SJF (Shortest Job First)";
-      explicacion.innerHTML="El más corto será el primero en ser atendido.";
+  let annadir = document.createElement("button");
+  annadir.setAttribute("id", "annadir");
+  annadir.setAttribute("onclick", "crearProceso");
+  annadir.innerHTML = "Añadir";
+  let lanzar = document.createElement("button");
+  lanzar.setAttribute("id", "lanzar");
+  lanzar.setAttribute("onclick", param + "()");
+  lanzar.innerHTML = "Lanzar";
 
-    }else if(param=="Round Robin"){
-      titulo.innerHTML="Round Robin";
-      explicacion.innerHTML="Se establece un intervalo de tiempo a cada proceso. Una vez este es agotado, pasa al siguiente.";
-    }else{}
-    subTarjeta.appendChild(titulo);
-    subTarjeta.appendChild(explicacion);
+  if (param == "metodoFIFO") {
+    titulo.innerHTML = "FIFO";
+    explicacion.innerHTML =
+      "El primer proceso en llegar, será el primero en ser atendido.";
+    num = 2;
+  } else if (param == "metodoSJF") {
+    titulo.innerHTML = "SJF (Shortest Job First)";
+    explicacion.innerHTML = "El más corto será el primero en ser atendido.";
+    num = 2;
+  } else if (param == "metodoRoundRobin") {
+    titulo.innerHTML = "Round Robin";
+    explicacion.innerHTML =
+      "Se establece un intervalo de tiempo a cada proceso. Una vez este es agotado, pasa al siguiente.";
+    num = 3;
+  } else {
+  }
+  subTarjeta.appendChild(titulo);
+  subTarjeta.appendChild(explicacion);
+  //cossas dek for
+  for (let i = 0; i < num; ++i) {
+    let dInput = document.createElement("div");
+    let lbl = document.createElement("label");
+    let inputTxt = document.createElement("input");
+    lbl.setAttribute("for", campos[i]);
+    lbl.innerHTML = campos[i] + ": ";
+    inputTxt.setAttribute("type", "text");
+    inputTxt.setAttribute("id", campos[i]);
+    inputTxt.setAttribute("name", campos[i]);
+
+    dInput.appendChild(lbl);
+    dInput.appendChild(inputTxt);
+    formulario.appendChild(dInput);
+  }
+
+  subTarjeta.appendChild(formulario);
+  subTarjeta.appendChild(annadir);
+  subTarjeta.appendChild(lanzar);
 }
-
-
-
-
 
 //********POO********//
 let qtum;
-function establecerQtum(pram){
-  qtum=param;
+function establecerQtum(pram) {
+  qtum = param;
 }
-//CREAR UN OBJETO CON CADA PROCESO INTRODUCIDO
-function crearProceso(llegada,duracion,prioridad,presente,enEjecucion){
-  //ints
-  this.llegda=llegada;
-  this.duracion=duracion;
-  this.prioridad=prioridad;
-  //Boleeans
-  this.presente=presente;
-  this.enEjecucion=enEjecucion
 
+//CREAR UN OBJETO CON CADA PROCESO INTRODUCIDO
+function crearProceso(llegada, duracion, presente, enEjecucion) {
+  //ints
+  this.llegda = llegada;
+  this.duracion = duracion;
+  //Boleeans
+  this.presente = presente;
+  this.enEjecucion = enEjecucion;
 }
